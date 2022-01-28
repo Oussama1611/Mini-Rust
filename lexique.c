4a66798546bc047052ec877d9054ca7be17b8483
Oussama1611 <oussamakrab2000@gmail.com>
2022-01-28 22:12:43 +0100

    Adding STRING TYPE

--- lexique.c
+++ lexique.c
@@ -4,28 +4,31 @@
 #include "lexique.h"
 #include "erreurs.h"
 #include "syntax.h"
-#define SIZE_TOKENS 18
+#define SIZE_TOKENS 19
 
 
 FILE* file;
 Symbol* symbol;
 TSym_Cour Sym_Cour;
+int COUNTER = 0;
 
 int arriere;
 const char* mots_cles[SIZE_TOKENS][2] = { {"fn","FN_TOKEN"}, {"const","CONST_TOKEN"},{"let","LET_TOKEN"}, 
 {"mut","MUT_TOKEN"},{"if","IF_TOKEN"},{"else","ELSE_TOKEN"},{"false","FALSE_TOKEN"},{"true","TRUE_TOKEN"},
 {"while","WHILE_TOKEN"},{"loop","LOOP_TOKEN"},{"for","FOR_TOKEN"},{"bool","BOOL_TOKEN"},{"in","IN_TOKEN"},{"continue","CONTI_TOKEN"},
-{"break","BREAK_TOKEN"},{"struct","STRUCT_TOKEN"},{"println","PRINT_TOKEN"},{"return","RETURN_TOKEN"}};
+{"break","BREAK_TOKEN"},{"struct","STRUCT_TOKEN"},{"String","STRING_TYPE_TOKEN"},{"println","PRINT_TOKEN"},{"return","RETURN_TOKEN"}};
 
-const int codes[SIZE_TOKENS] = {FN_TOKEN, CONST_TOKEN, LET_TOKEN, MUT_TOKEN, IF_TOKEN, ELSE_TOKEN, FALSE_TOKEN, TRUE_TOKEN, WHILE_TOKEN, LOOP_TOKEN, FOR_TOKEN, BOOL_TOKEN, IN_TOKEN, CONTI_TOKEN, BREAK_TOKEN, STRUCT_TOKEN, PRINT_TOKEN, RETURN_TOKEN};
+const int codes[SIZE_TOKENS] = {FN_TOKEN, CONST_TOKEN, LET_TOKEN, MUT_TOKEN, IF_TOKEN, ELSE_TOKEN, FALSE_TOKEN, TRUE_TOKEN, WHILE_TOKEN, LOOP_TOKEN, FOR_TOKEN, BOOL_TOKEN, IN_TOKEN, CONTI_TOKEN, BREAK_TOKEN, STRUCT_TOKEN, STRING_TYPE_TOKEN, PRINT_TOKEN, RETURN_TOKEN};
 
 void symbol_suivant(){
     if(arriere){
         symbol->c = arriere;           
         arriere = 0;
         return;
+    } else {
+        symbol->c = fgetc(file);
+        if(symbol->c == '\n') COUNTER++;
     }
-    symbol->c = fgetc(file);
 }
 
 void revenir_arriere(){
@@ -50,7 +53,7 @@ void assign_nom_token(char* mot){
         }
     
     } 
-    printf("%s\t",mot);
+    //printf("%s\t",mot);
 }
 
 void lire_mot(){
@@ -70,7 +73,7 @@ void lire_mot(){
             revenir_arriere();
             Sym_Cour.CODE=NUM_TYPE_TOKEN;
             strcpy(Sym_Cour.NOM,"NUM_TYPE_TOKEN");
-            printf("%c%s\t",C[0],nombre);
+            //printf("%c%s\t",C[0],nombre);
             free(nombre);
             return;
 
@@ -103,7 +106,7 @@ void lire_nombre(){
     Sym_Cour.CODE=NUM_TOKEN;
     strcpy(Sym_Cour.NOM,"NUM_TOKEN");
     Sym_Cour.val=atoi(nombre); //  on ajoute la valeur de nombre apres consideration des regles semantiques
-    printf("%s\t",nombre); 
+    //printf("%s\t",nombre); 
     free(nombre);
    
 }
@@ -128,7 +131,7 @@ void lire_special(){
     if (symbol->c==')'){
         Sym_Cour.CODE = PF_TOKEN;
         strcpy(Sym_Cour.NOM,"PF_TOKEN");
-        printf(")\t");
+        //printf(")\t");
         return;
     }
     if(symbol->c=='('){
@@ -136,81 +139,81 @@ void lire_special(){
         if (symbol->c==')'){
             Sym_Cour.CODE = UNIT_TOKEN;
             strcpy(Sym_Cour.NOM,"UNIT_TOKEN");
-            printf("()\t");
+            //printf("()\t");
             return;
         } else {
             revenir_arriere();
             Sym_Cour.CODE = PO_TOKEN;
             strcpy(Sym_Cour.NOM,"PO_TOKEN");
-            printf("(\t");
+            //printf("(\t");
             return;
         }
     }
     if(symbol->c=='{'){
         Sym_Cour.CODE = ACCOLO_TOKEN;
         strcpy(Sym_Cour.NOM,"ACCOLO_TOKEN");
-        printf("{\t");
+        //printf("{\t");
         return;
     }
     if (symbol->c=='}'){
         Sym_Cour.CODE = ACCOLF_TOKEN;
         strcpy(Sym_Cour.NOM,"ACCOLF_TOKEN");
-        printf("}\t");
+        //printf("}\t");
         return;
     } 
     if(symbol->c==';'){
         Sym_Cour.CODE = PV_TOKEN;
         strcpy(Sym_Cour.NOM,"PV_TOKEN");
-        printf(";\t");
+        //printf(";\t");
         return;
     }
     if(symbol->c=='['){
         Sym_Cour.CODE = CROCHO_TOKEN;
         strcpy(Sym_Cour.NOM,"CROCHO_TOKEN");
-        printf("[\t");
+        //printf("[\t");
         return;
     }
     if(symbol->c==']'){
         Sym_Cour.CODE = CROCHF_TOKEN;
         strcpy(Sym_Cour.NOM,"CROCHF_TOKEN");
-        printf("]\t");
+        //printf("]\t");
         return;
     }
 
     if(symbol->c=='+'){
         Sym_Cour.CODE = PLUS_TOKEN;
         strcpy(Sym_Cour.NOM,"PLUS_TOKEN");
-        printf("+\t");
+        //printf("+\t");
         return;
     }
     if(symbol->c=='*'){
         Sym_Cour.CODE = MULT_TOKEN;
         strcpy(Sym_Cour.NOM,"MULTI_TOKEN");
-        printf("*\t");
+        //printf("*\t");
         return;
     }
     if(symbol->c=='/'){ 
         Sym_Cour.CODE = DIV_TOKEN;
         strcpy(Sym_Cour.NOM,"DIV_TOKEN");
-        printf("/\t");
+        //printf("/\t");
         return;
     }
     if(symbol->c==','){
         Sym_Cour.CODE = VIR_TOKEN;
         strcpy(Sym_Cour.NOM,"VIR_TOKEN");
-        printf(",\t");
+        //printf(",\t");
         return;
     }
     if(symbol->c =='"'){
         Sym_Cour.CODE = GUILL_TOKEN;
         strcpy(Sym_Cour.NOM,"GUILL_TOKEN");
-        printf("\"\t");
+        //printf("\"\t");
         return;
     }
     if(symbol->c=='%'){
         Sym_Cour.CODE = REST_TOKEN;
         strcpy(Sym_Cour.NOM,"REST_TOKEN");
-        printf("%c\t",symbol->c);
+        //printf("%c\t",symbol->c);
         return;
     }
     if (symbol->c=='.'){
@@ -218,20 +221,20 @@ void lire_special(){
         if(symbol->c=='.'){
             Sym_Cour.CODE = RANG_TOKEN;
             strcpy(Sym_Cour.NOM,"RANG_TOKEN");
-            printf("..\t");
+            //printf("..\t");
             return;
         } else {
             revenir_arriere();
             Sym_Cour.CODE = PT_TOKEN;
             strcpy(Sym_Cour.NOM,"PT_TOKEN");
-            printf(".\t");
+            //printf(".\t");
             return;
         }
     }
     if(symbol->c==':'){
         Sym_Cour.CODE = COL_TOKEN;
         strcpy(Sym_Cour.NOM,"COL_TOKEN");
-        printf(":\t");
+        //printf(":\t");
         return;
     }
 
@@ -240,14 +243,14 @@ void lire_special(){
         if(symbol->c=='='){
             Sym_Cour.CODE = INFEG_TOKEN;
             strcpy(Sym_Cour.NOM,"INFEG_TOKEN");
-            printf("<=\t");
+            //printf("<=\t");
             return;
         }
         else{
             revenir_arriere();
             Sym_Cour.CODE=INF_TOKEN;
             strcpy(Sym_Cour.NOM,"INF_TOKEN");
-            printf("<\t");
+            //printf("<\t");
             return;
         }
         
@@ -257,13 +260,13 @@ void lire_special(){
         if(symbol->c=='>'){
             Sym_Cour.CODE = RETURN_TYPE_TOKEN;
             strcpy(Sym_Cour.NOM,"RETURN_TYPE_TOKEN");
-            printf("->\t");
+            //printf("->\t");
             return;
         } else {
             revenir_arriere();
             Sym_Cour.CODE = MOINS_TOKEN;
             strcpy(Sym_Cour.NOM,"MOINS_TOKEN");
-            printf("-\t");
+            //printf("-\t");
             return;
         }
     }
@@ -272,13 +275,13 @@ void lire_special(){
         if(symbol->c=='='){
             Sym_Cour.CODE = COMP_TOKEN;
             strcpy(Sym_Cour.NOM,"COMP_TOKEN");
-            printf("==\t");
+            //printf("==\t");
             return;
         } else {
             revenir_arriere();
             Sym_Cour.CODE = AFF_TOKEN;
             strcpy(Sym_Cour.NOM,"AFF_TOKEN");
-            printf("=\t");
+            //printf("=\t");
             return;
         }
     }  
@@ -287,13 +290,13 @@ void lire_special(){
         if(symbol->c=='='){
             Sym_Cour.CODE = DIFF_TOKEN;
             strcpy(Sym_Cour.NOM,"DIFF_TOKEN");
-            printf("!=\t");
+            //printf("!=\t");
             return;
         } else {
             revenir_arriere();
             Sym_Cour.CODE = NON_TOKEN;
             strcpy(Sym_Cour.NOM,"NON_TOKEN");
-            printf("!\t");
+            //printf("!\t");
             return;
         }
     }    
@@ -302,14 +305,14 @@ void lire_special(){
         if(symbol->c=='='){
             Sym_Cour.CODE = SUPEG_TOKEN;
             strcpy(Sym_Cour.NOM,"SUPEG_TOKEN");
-            printf(">=\t");
+            //printf(">=\t");
             return;
         }
         else{
             revenir_arriere();
             Sym_Cour.CODE=SUP_TOKEN;
             strcpy(Sym_Cour.NOM,"SUP_TOKEN");
-            printf(">\t");
+            //printf(">\t");
             return;
         }
         
@@ -317,8 +320,8 @@ void lire_special(){
 
 }
 
-void print_token(){
-    printf("%s\n",Sym_Cour.NOM);
+void print_token(){ // fonction pour afficher les tokens 
+    //printf("%s\n",Sym_Cour.NOM);
 }
 
 void token_suivant(){
@@ -328,24 +331,24 @@ void token_suivant(){
     sauter_espace();
     if(ispunct(symbol->c)){
         lire_special();
-        print_token();
+        //print_token();
         return;
     }
     if(isdigit(symbol->c)){
         lire_nombre();
-        print_token();
+        //print_token();
         return;  
     }
     if(isalpha(symbol->c)){
         lire_mot();
-        print_token();
+        //print_token();
         return;
     }
     if (symbol->c==EOF)
     {
         Sym_Cour.CODE=END_TOKEN;
         strcpy(Sym_Cour.NOM,"END_TOKEN");
-        print_token();
+        //print_token();
         return;
     }
 }    
